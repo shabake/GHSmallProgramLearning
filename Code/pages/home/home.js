e0779948600cb4b54d632098024f79b021a939df
shabake <45329453@qq.com>
2019-11-24 19:17:51 +0800

    获取数据

--- Code/pages/home/home.js
+++ Code/pages/home/home.js
@@ -15,18 +15,15 @@ Page({
           success: (res) => {
             let city = res.data.result.addressComponent.city;
             city = city.substring(0, city.length - 1);
-            console.log('获取位置');
             success(city);
           },
           fail: (erroe) => {
-            console.log(erroe);
           },
           complete: () => { }
         });
       },
 
       fail: () => {
-        console.log('获取位置失败');
       },
       complete: () => {
 
@@ -34,31 +31,73 @@ Page({
     });
   },
 
-  /**加载数据 */
-  loadData: function (city) {
-    wx.request({
-      url: 'https://douban.uieee.com/v2/movie/in_theaters',
-      data: { city: city },
-      header: { 'content-type': 'json' },
-      method: 'GET',
-      dataType: 'json',
-      responseType: 'text',
-      success: (result) => {
-        console.log(result);
-        this.setData({
-          movies : result.data.subjects /**取出数据 告诉页面重新加载数据 */
-        })
+  data: {
+    allMovies: [
+      {
+        title: '影院热映',
+        url: 'v2/movie/in_theaters',
+        movies: [],
       },
-      fail: () => { },
+      {
+        title: '新片榜',
+        url: 'v2/movie/new_movies',
+        movies: [],
+      },
+      {
+        title: '口碑榜',
+        url: 'v2/movie/weekly',
+        movies: [],
+      },
+      {
+        title: '北美票房榜',
+        url: 'v2/movie/us_box',
+        movies: [],
+      },
+      {
+        title: 'Top250',
+        url: 'v2/movie/top250',
+        movies: [],
+      },
+    ],
+  },
+  onLoad: function () {
+    this.loadAllData();
+  },
+
+  loadAllData: function () {
+    this.loadCity((city) => {
+      this.loadOtherData(0, { city: city });
+    });
+    this.loadOtherData(0);
+    this.loadOtherData(1);
+    this.loadOtherData(2);
+    this.loadOtherData(3);
+    this.loadOtherData(4);
+  },
+
+  loadOtherData: function (index, dict) {
+    let object = this.data.allMovies[index];
+    wx.request({
+      url: wx.db.url(object.url),
+      data: dict,
+      header: { 'content-type': 'json' },
+      success: (result) => {
+        const movies = result.data.subjects;
+        let obj = this.data.allMovies[index];
+        for (let index = 0; index < movies.length; index++) {
+          let movie = movies[index].subject || movies[index];
+          obj.movies.push(movie);
+        }
+        this.setData(this.data);
+      },
+      fail: (err) => {
+        console.log(err);
+      },
       complete: () => { }
     });
   },
-
-  data:{
-    movies:[]
+  updateMovies: function (movies) {
+    // let stars = parseInt(movies.)
   },
 
-  onLoad: function () {
-    this.loadCity(this.loadData);
-  },
 })
