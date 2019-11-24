20452c57641a7aef299c07460c7056f369617a25
shabake <45329453@qq.com>
2019-11-24 12:56:08 +0800

    获取定位信息

--- Code/pages/home/home.js
+++ Code/pages/home/home.js
@@ -1,66 +1,58 @@
 // pages/home/home.js
 Page({
+  
+  loadCity: function(success) {
+    wx.getLocation({
+      success: (res) => {
+        wx.request({
+          url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+          data: {
+            output: 'json',
+            coordtype: 'wgs84ll',
+            ak: 'LhikkZ78GVuG8wFijkN0dhvVUfeYYv02',
+            location: res.latitude + ',' + res.longitude
+          },
+          success: (res) => {
+            let city = res.data.result.addressComponent.city;
+            city = city.substring(0, city.length - 1);
+            console.log('获取位置');
+            success(city);
+          },
+          fail: (erroe) => {
+            console.log(erroe);
+          },
+          complete: () => { }
+        });
+      },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+      fail: () => {
+        console.log('获取位置失败');
+      },
+      complete: () => {
 
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  /**加载数据 */
+  loadData: function(city) {
+    wx.request({
+      url: 'https://douban.uieee.com/v2/movie/in_theaters',
+      data: {city:city},
+      header: {'content-type':'json'},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result) => {
+        console.log(result);
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+      
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onLoad: function () {
+    this.loadCity(this.loadData);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
